729a1ad87eb812d185827cb1b12f70d25daebcf6
User_1 <user_1@User-1s-MBP.attlocal.net>
2023-08-18 09:44:44 -0700

    use chat prompt and model

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,16 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PineconeClient } from "@pinecone-database/pinecone";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
 import * as Ably from 'ably'
 import { uuid } from 'uuidv4';
-import { ConversationalRetrievalQAChain } from "langchain/chains";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { LLMChain } from "langchain/chains";
 import { ConversationLog } from "./conversationLog";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
+import {
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+  } from "langchain/prompts";
 let client: PineconeClient | null = null
 
-const PINECONE_NAME_SPACE = 'mentalHealth-help'; //namespace is optional for your vectors
 
 type PageSource = {
     pageContent: string,
@@ -28,104 +30,113 @@ const initPineconeClient = async () => {
 }
 
 const ably = new Ably.Realtime({ key: process.env.ABLY_API_KEY})
-const CONDENSE_PROMPT = `Given the following conversation and a answer, rephrase the answer to be a standalone question.
+
+const SUMMARY_PROMPT = `Below is a conversation between a therapist and a patient. Analyze the conversation to see if the root cause of the patient's (user) problem has been identified. The root cause of a problem should not be a surface level realization, it should be the true deeper underlying cause of the problem, not the first cause that comes up. An example of a root cause would be, an underlying feeling such as "I'm not good enough" or "that's what my parents did to me". An example of something that is a symptom and not a root cause of a problem is, "I used to be healthy but I'm not anymore". If you identify multiple problems, then make a list of all problems. If the root cause of the problem has been identified, reply with "True". If not, reply with "False". Reply following the template below and give no additional words or characters.
+Example of response:
+Problem 1 - [short description of problem] - True
+Problem 2 - [short description of problem] - False
+
 
 Chat History:
 {chat_history}
-Follow Up Input: {question}
-Standalone question:`;
+`;
 
-const QA_PROMPT = `You are an expert therapist trained in Internal Family Systems therapy. Help me explore my internal world and use principles from internal family systems therapy. Help me understand the different parts of myself by asking thoughtful questions. Engage with me in a supportive and understanding manner, mirroring the tone and cadence of a compassionate listener. Dive deep into my feelings, ask clarifying questions, and provide insights that help me navigate my emotions. Analyze the conversation to uncover deep-seated motivations, emotional drivers, and underlying reasons. Go beyond immediate and observable causes to probe into potential psychological, societal, or personal pressures influencing my decisions.
 
-Your goal is to understand what my problem is and help me uncover the root cause of my problem. The root cause of a problem should not be a surface level realization, it should be the true deeper underlying cause of the problem, not the first cause that comes up. An example of a root cause would be, an underlying feeling such as "I'm not good enough" or "that's what my parents did to me". An example of something that is a symptom and not a root cause of a problem is, "I used to be healthy but I'm not anymore". If you sense that I’m giving you a symptom response, ask more probing questions. If you sense that you’ve identified the root cause of the problem, reply with a supportive and compassionate response and give me a summary of the situation and make me feel better.
 
-Provide gentle guidance and insights when appropriate, without overwhelming me. Your responses should be supportive, non-judgmental, and foster self-awareness and healing. Only ask one question at a time and provide me with fewer disclaimers.
 
-Sometimes give me an example of advice on how I could look at my situation differently. Don't ever give me a large list of recommendations.
 
-Chat History:
-{chat_history}
+// const checkIssueSummarizer =async (model:any, vectorStore:any, nonStreamingModel:any,chat_history:string) => {
+    
+//     const chain = ConversationalRetrievalQAChain.fromLLM(
+//         model,
+//         vectorStore.asRetriever(),
+//         { 
+//             qaTemplate: SUMMARY_PROMPT,
+//             // questionGeneratorTemplate: CONDENSE_PROMPT,
+//             returnSourceDocuments: true,
+//             questionGeneratorChainOptions: {
+//                 llm: nonStreamingModel
+//             }        
+//         }
+//     )
 
-patient: {question}
-Therapist:`;
+//     const response = await chain.call({  chat_history })
 
+
+
+// }
 const handleRequest = async ({ prompt, userId, source, streaming }: { prompt: string, userId: string, source: boolean, streaming: boolean }) => {
    if(!client) {
     await initPineconeClient()
    }
 
    try {
-     const channel = ably.channels.get(userId)
-     const interactionId = uuid()
+        const channel = ably.channels.get(userId)
+        const interactionId = uuid()
 
-     const conversationLog = new ConversationLog(userId)
-     const conversationHistory = await conversationLog.getConverstion({ limit: 10})
-     await conversationLog.addEntry({ entry: prompt, speaker: "user"})
-
-     const pineconeIndex = client!.Index(process.env.PINECONE_INDEX!)
-     
-     channel.publish({
+        const conversationLog = new ConversationLog(userId)
+        const conversationHistory = await conversationLog.getConverstion({ limit: 10})
+        await conversationLog.addEntry({ entry: prompt, speaker: "user"})
+        
+        channel.publish({
         data: {
             event: "status",
             message: "Finding matches..."
         }
-     })
+        })
 
-     const vectorStore = await PineconeStore.fromExistingIndex(
-        new OpenAIEmbeddings(),
-        { 
-            pineconeIndex,
-            textKey: 'text',
-            namespace: PINECONE_NAME_SPACE,
-        }
-     )
+        const model = new ChatOpenAI({
+            temperature: 0,
+            streaming,
+            modelName: 'gpt-4', //change this to gpt-4 if you have access
+            callbacks: [{
+                async handleLLMNewToken(token) {
+                    channel.publish({
+                        data: {
+                            event: "response",
+                            token,
+                            interactionId 
+                        }
+                    })
+                },
+                async handleLLMEnd() {
+                    channel.publish({
+                        data: {
+                            event: "responseEnd"
+                        }
+                    })
+                }
+            }]         
+        });
+        const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+            SystemMessagePromptTemplate.fromTemplate(
+            `You are an expert therapist trained in Internal Family Systems therapy. Help me explore my internal world and use principles from internal family systems therapy. Help me understand the different parts of myself by asking thoughtful questions. Engage with me in a supportive and understanding manner, mirroring the tone and cadence of a compassionate listener. Dive deep into my feelings, ask clarifying questions, and provide insights that help me navigate my emotions. Analyze the conversation to uncover deep-seated motivations, emotional drivers, and underlying reasons. Go beyond immediate and observable causes to probe into potential psychological, societal, or personal pressures influencing my decisions.
 
-     const model = new ChatOpenAI({
-         temperature: 0,
-         streaming,
-         modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-         callbacks: [{
-            async handleLLMNewToken(token) {
-                channel.publish({
-                    data: {
-                        event: "response",
-                        token,
-                        interactionId 
-                    }
-                })
-            },
-            async handleLLMEnd() {
-                channel.publish({
-                    data: {
-                        event: "responseEnd"
-                    }
-                })
-            }
-        }]         
-    })
-   
-     const nonStreamingModel = new ChatOpenAI({
-        temperature: 0,
-        modelName: 'gpt-4', //change this to gpt-4 if you have access
-     })
+            Your goal is to understand what my problem is and help me uncover the root cause of my problem. The root cause of a problem should not be a surface level realization, it should be the true deeper underlying cause of the problem, not the first cause that comes up. An example of a root cause would be, an underlying feeling such as "I'm not good enough" or "that's what my parents did to me". An example of something that is a symptom and not a root cause of a problem is, "I used to be healthy but I'm not anymore". If you sense that I’m giving you a symptom response, ask more probing questions. If you sense that you’ve identified the root cause of the problem, reply with a supportive and compassionate response and give me a summary of the situation and make me feel better.
+            
+            Provide gentle guidance and insights when appropriate, without overwhelming me. Your responses should be supportive, non-judgmental, and foster self-awareness and healing. Only ask one question at a time and provide me with fewer disclaimers.
+            
+            Sometimes give me an example of advice on how I could look at my situation differently. Don't ever give me a large list of recommendations and dont every mention.
+            
+            Chat History:
+            {chat_history}`
+            ),
+            HumanMessagePromptTemplate.fromTemplate("{question}"),
+        ]);
+        
+        const chain = new LLMChain({
+            llm: model,
+            prompt: chatPrompt,
+        });
+        
 
-     const chain = ConversationalRetrievalQAChain.fromLLM(
-        model,
-        vectorStore.asRetriever(),
-        { 
-            qaTemplate: QA_PROMPT,
-            // questionGeneratorTemplate: CONDENSE_PROMPT,
-            returnSourceDocuments: true,
-            questionGeneratorChainOptions: {
-                llm: nonStreamingModel
-            }        
-        }
-     )
+        let chat_history =  conversationHistory.join("\n")
 
-     let chat_history =  conversationHistory.join("\n")
-     const response = await chain.call({ question: prompt, chat_history })
+        //  let resp = checkIssueSummarizer(model,vectorStore,nonStreamingModel,chat_history)
 
-     if(!streaming) {
+        const response = await chain.call({ question: prompt, chat_history })
+
+        if(!streaming) {
         channel.publish({
             data: {
                 event: "response",
@@ -133,35 +144,35 @@ const handleRequest = async ({ prompt, userId, source, streaming }: { prompt: st
                 interactionId 
             }
         })
-     }
+        }
 
-     if(source) {
-        const pageContents: string[] = []
+        if(source) {
+            const pageContents: string[] = []
 
-        let index = 1
-        response.sourceDocuments.forEach((source: PageSource) => {
-            const { pageContent, metadata: { url }} = source
-        
-            if(!pageContents.includes(pageContent)){
-                const token = `<br/><b>Source #${index}</b>
-                                    <br/>${pageContent}
-                                    <br/><a href="${url}" target="_blank">${url}</a>` 
+            let index = 1
+            response.sourceDocuments.forEach((source: PageSource) => {
+                const { pageContent, metadata: { url }} = source
             
-                channel.publish({
-                    data: {
-                        event: "response",
-                        token: "<br/>" + token,
-                        interactionId 
-                    }
-                })
-            
-                pageContents.push(pageContent)
-                index++
-            }
-        });
-    }
+                if(!pageContents.includes(pageContent)){
+                    const token = `<br/><b>Source #${index}</b>
+                                        <br/>${pageContent}
+                                        <br/><a href="${url}" target="_blank">${url}</a>` 
+                
+                    channel.publish({
+                        data: {
+                            event: "response",
+                            token: "<br/>" + token,
+                            interactionId 
+                        }
+                    })
+                
+                    pageContents.push(pageContent)
+                    index++
+                }
+            });
+        }
 
-     await conversationLog.addEntry({ entry: response.text, speaker: "bot" })
+        await conversationLog.addEntry({ entry: response.text, speaker: "bot" })
 
     } catch(error) { 
        console.error(error)
